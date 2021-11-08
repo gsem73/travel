e7e8c4d4d46f02771171ac84ad55c8678d4cc506
sga73 <gh5t@yandex.ru>
2021-11-08 22:32:21 +0300

    Добавлен css файл

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 import gulp from 'gulp';
 import htmlmin from 'gulp-htmlmin';
 import sync from 'browser-sync';
+import rename from 'gulp-rename';
+import replace from 'gulp-replace';
 
 const html = function() {
   return gulp.src('source/*.html')
+    .pipe(replace('style.css', 'style.min.css') )
     .pipe(htmlmin({
       removeComments: true,
       collapseWhitespace: true,
@@ -12,13 +15,22 @@ const html = function() {
     .pipe(sync.stream());
 };
 
+const css = function() {
+  return gulp.src('source/css/style.css')
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(sync.stream());
+};
+
 const watch = function() {
   gulp.watch('source/*.html', html);
+  gulp.watch('source/**/*.css', css);
 };
 
 const server = function() {
   sync.init({
     notify: false,
+    ui: false,
     server: {
       baseDir: './build'
     }
@@ -27,6 +39,7 @@ const server = function() {
 
 export default gulp.series(
   html,
+  css,
   gulp.parallel(
     watch,
     server,
